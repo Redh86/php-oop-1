02a1763998d5cd9c3fdaf0625022aed67b375f54
Dario Tedeschi <dario.tedeschi86@hotmail.it>
2021-05-10 17:04:28 +0200

    3 film istanziati

--- index.php
+++ index.php
@@ -7,13 +7,30 @@ class Movie {
     public $anno;
 }
 
-$starWars = new Movie();
-$starWars->titolo = "L'impero colpisce ancora";
-$starWars->regista = "Irvin Kershner";
-$starWars->genere = "Fantascienza";
-$starWars->anno = "1980";
 
-var_dump($starWars);
+$speranza = new Movie();
+$speranza->titolo = "Una nuova speranza";
+$speranza->regista = "George Lucas";
+$speranza->genere = "Fantascienza";
+$speranza->anno = "1977";
+
+var_dump($speranza);
+
+$impero = new Movie();
+$impero->titolo = "L'impero colpisce ancora";
+$impero->regista = "Irvin Kershner";
+$impero->genere = "Fantascienza";
+$impero->anno = "1980";
+
+var_dump($impero);
+
+$ritorno = new Movie();
+$ritorno->titolo = "Il ritorno dello Jedi";
+$ritorno->regista = "Richard Marquand";
+$ritorno->genere = "Fantascienza";
+$ritorno->anno = "1983";
+
+var_dump($ritorno);
 
 
 
